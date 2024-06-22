37ba497c09e5bf9099b1b345c8340aba92537653
Siddharth Rayabharam <maitreya2954@gmail.com>
2024-06-22 18:40:55 -0400

    Implement fault tolerance - Circuit Breaker

---
+++ src/main/java/org/systems/scheduler/service/TaskExecutionService.java
@@ -0,0 +1,50 @@
+package org.systems.scheduler.service;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.systems.scheduler.strategy.TaskExecutionStrategy;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
+@Component
+class TaskExecutionService {
+	
+	@Autowired
+	private Map<String, TaskExecutionStrategy> taskExecutionStrategies;
+	
+	@CircuitBreaker(name="taskExecution", fallbackMethod = "handleTaskExecutionFailure")
+	public void executeTask(String taskId) {
+		
+		System.out.println("Executing task: " + taskId);
+		
+		// Extract task type from taskId (assuming taskId format: typeX-taskId)
+        String taskType = taskId.split("-")[0];
+        
+        // Get appropriate strategy based on task type and execute task
+        TaskExecutionStrategy strategy = taskExecutionStrategies.get(taskType);
+        if (strategy != null) {
+        	try {
+        		strategy.executeTask(taskId);
+			} catch (Exception e) {
+                // Handle task execution failure
+                System.out.println("Failed to execute task: " + taskId);
+			}
+            
+        } else {
+            // Handle unknown task type
+            System.out.println("Unknown task type: " + taskType);
+        }
+        
+        //Simulate failure
+        if (Math.random() < 0.5) {
+        	throw new RuntimeException("Task execution failed for task: " + taskId);
+        }
+	}
+	
+	public void handleTaskExecutionFailure(String taskId, Throwable throwable) {
+		System.out.println("Task execution failed for task: " + taskId);
+	}
+	
+}
--- src/main/java/org/systems/scheduler/service/TaskSubscriber.java
+++ src/main/java/org/systems/scheduler/service/TaskSubscriber.java
@@ -1,18 +1,15 @@
 package org.systems.scheduler.service;
 
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
-import org.systems.scheduler.strategy.TaskExecutionStrategy;
 
 public class TaskSubscriber implements MessageListener {
 
 	private static final String TASK_CHANNEL = "task-channel";
 	
 	@Autowired
-	private Map<String, TaskExecutionStrategy> taskExecutionStrategies;
+	private TaskExecutionService taskExecutionService;
 
 	@Override
 	public void onMessage(Message message, byte[] pattern) {
@@ -20,17 +17,7 @@ public class TaskSubscriber implements MessageListener {
 		String taskId = new String(message.getBody());
 		System.out.println("Received task ID: " + taskId);
 		
-        // Extract task type from taskId (assuming taskId format: typeX-taskId)
-        String taskType = taskId.split("-")[0];
-        
-        // Get appropriate strategy based on task type and execute task
-        TaskExecutionStrategy strategy = taskExecutionStrategies.get(taskType);
-        if (strategy != null) {
-            strategy.executeTask(taskId);
-        } else {
-            // Handle unknown task type
-            System.out.println("Unknown task type: " + taskType);
-        }
+        taskExecutionService.executeTask(taskId);
         
 	}
 	
